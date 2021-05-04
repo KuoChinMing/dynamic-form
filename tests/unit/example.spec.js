aae37252a1d2d3cd9dd9608e18b31c374d240ba0
ChinMingKuo <theseus013059@gmail.com>
2021-05-04 09:26:31 +0800

    test (example.spec.js): 測試撰寫單元測試

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,57 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import OperationPanel from "@/components/OperationPanel.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+describe("Operation Panel", () => {
+  it("find parent node", () => {
+    const template = {
+      type: "box",
+      id: 0,
+      contents: [
+        {
+          type: "box",
+          id: 1,
+          contents: [
+            {
+              type: "textField",
+              id: 2,
+              bindingKey: "textField1",
+            },
+            {
+              type: "box",
+              id: 3,
+              contents: [
+                {
+                  type: "textField",
+                  id: 4,
+                  bindingKey: "textField2",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "box",
+          id: 5,
+          contents: [
+            {
+              type: "textField",
+              id: 6,
+              bindingKey: "textField3",
+            },
+          ],
+        },
+      ],
+    };
+    const bindingData = {
+      textField1: null,
+      textField2: null,
+      textField3: null,
+    };
+    const wrapper = shallowMount(OperationPanel, {
+      propsData: { template, bindingData },
     });
-    expect(wrapper.text()).to.include(msg);
+    
+    expect(wrapper.vm.findParentNode(template.contents[0])).to.equal(template);
   });
 });
